f0867628091360e7bc92566ab8cdf951b6293c9d
d_liyanage <admliyanage@gmail.com>
2020-07-17 15:39:16 +0530

    init commit

---
+++ src/main/java/com/tolltracker/tolltrackerapi/TollTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tolltracker.tolltrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tolltracker/tolltrackerapi/TollTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolltracker.tolltrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
